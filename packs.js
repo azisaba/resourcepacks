a449b8f2b4f2708cdfc227bc52960293954ddb4d
ゆき <33691365+YukiLeafX@users.noreply.github.com>
2020-08-01 10:33:13 +0900

    Add files via upload

---
+++ packs.js
@@ -0,0 +1,40 @@
+$(function () {
+    function getPackVersion(packFormat) {
+        switch (packFormat) {
+            case 1:
+                return "1.6.1–1.8.9";
+            case 2:
+                return "1.9–1.10.2";
+            case 3:
+                return "1.11–1.12.2";
+            case 4:
+                return "1.13–1.14.4";
+            case 5:
+                return "1.15–1.16.1";
+        }
+    }
+
+    function createPackCard(fileName, description, sha1, packFormat, version) {
+        return $('<div class="container py-2"><div class="row"><div class="card w-100"><div class="card-header">' + fileName + '</div><div class="card-body"><p class="card-text">' + description + '</p><ul class="list-group list-group-flush"><li class="list-group-item">SHA1: ' + sha1 + '</li><li class="list-group-item">対応バージョン: ' + version + ' (' + packFormat + ')</li></ul><a href="' + fileName + '" class="btn btn-primary float-right">ダウンロード</a></div></div></div></div>')
+    }
+
+    $.getJSON("packs.json", function (json) {
+        for (var i = 0; i < json.length; i++) {
+            let fileName = json[i].filename
+            let description = json[i].description
+            let sha1 = json[i].sha1
+            let packFormat = json[i].format
+            let version = getPackVersion(packFormat)
+
+            createPackCard(fileName, description, sha1, packFormat, version).appendTo('#packs');
+        }
+    });
+
+    // let fileName = "lgw.zip";
+    // let description = "lgwのリソースパックです。";
+    // let sha1 = "aaaaaaaaaabbbbbbbbbbccccccccccdddddddddd";
+    // let packFormat = 3;
+    // let version = getPackVersion(packFormat)
+
+    // createPackCard(fileName, description, sha1, packFormat, version).appendTo('#packs');
+});
