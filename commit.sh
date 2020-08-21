8ec7e201529c04f295372f7a2e4cd51ab8bf6a49
YukiLeafX <yukileafx@gmail.com>
2020-08-21 14:57:56 +0900

    コミットの修正

--- commit.sh
+++ commit.sh
@@ -8,4 +8,4 @@ git branch -D "$branch" || :
 git switch --orphan "$branch"
 git reset --soft origin/"$branch" -- || :
 git add -A
-git commit -m '💡 このコミットは自動生成されました。' --author 'github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>' || :
+git -c user.name='github-actions[bot]' -c user.email='41898282+github-actions[bot]@users.noreply.github.com' commit -m '💡 このコミットは自動生成されました。' || :
